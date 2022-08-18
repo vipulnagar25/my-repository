b06a9c20de14ca902a9a3e9e60c90c758e4845de
Vipul Nagar <vipulnagar25@gmail.com>
2022-08-18 10:21:59 +0530

    enhance some issue

--- core-java/src/com/collection/linkedList/ProblemsOnLinkedList.java
+++ core-java/src/com/collection/linkedList/ProblemsOnLinkedList.java
@@ -27,26 +27,50 @@ public class ProblemsOnLinkedList {
         return false;
     }
 
+    public void removeFromNthElement(MyLinkedList linkedList, int n) {
+        Node fast = linkedList.head;
+        Node slow = linkedList.head;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            return;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+
+    }
+
 
     public static void main(String[] args) {
         ProblemsOnLinkedList problems = new ProblemsOnLinkedList();
         MyLinkedList linkedList = new MyLinkedList();
         linkedList.add("1");
-        linkedList.add("1");
+        // linkedList.add("1");
         linkedList.add("2");
-        linkedList.add("2");
+        // linkedList.add("2");
         linkedList.add("3");
-        linkedList.add("3");
-        linkedList.add("3");
-        linkedList.add("3");
+        // linkedList.add("3");
+        // linkedList.add("3");
+        // linkedList.add("3");
         linkedList.add("4");
-        linkedList.add("4");
+        // linkedList.add("4");
         linkedList.add("5");
-       // linkedList.printLinkedList();
-        problems.removeDuplicateFromSortedList(linkedList);
+        // linkedList.printLinkedList();
+        //  problems.removeDuplicateFromSortedList(linkedList);
+        // link
+        // edList.printLinkedList();
         linkedList.printLinkedList();
-        System.out.println(problems.isCyclic(linkedList));
+        problems.removeFromNthElement(linkedList, 2);
+        linkedList.printLinkedList();
 
 
     }
 }
+
