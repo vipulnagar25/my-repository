770a186e445c0fdbccca2475c2308c08f6c19b3f
Vipul Nagar <vipulnagar25@gmail.com>
2022-08-23 20:26:00 +0530

    enhance some issue

---
+++ core-java/src/com/problem/MissingNumberInASortedArray.java
@@ -0,0 +1,31 @@
+package com.problem;
+
+public class MissingNumberInASortedArray {
+    static int search(int ar[], int size) {
+        // Extreme cases
+        if (ar[0] != 1)
+            return 1;
+        if (ar[size - 1] != (size + 1))
+            return size + 1;
+
+        int a = 0, b = size - 1;
+        int mid = 0;
+        while ((b - a) > 1) {
+       // while (a<b) {
+            mid = (a + b) / 2;
+            if ((ar[a] - a) != (ar[mid] - mid))
+                b = mid;
+            else if ((ar[b] - b) != (ar[mid] - mid))
+                a = mid;
+        }
+        return (ar[a] + 1);
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        int ar[] = {1, 2, 3, 4, 5, 6, 8};
+        int size = ar.length;
+        System.out.println("Missing number: "
+                + search(ar, size));
+    }
+}
