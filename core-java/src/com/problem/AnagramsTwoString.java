7bd62649cd8b3cd89d54217d4d77dd8f819917b3
Vipul Nagar <vipulnagar25@gmail.com>
2022-10-10 12:59:13 +0530

    Thread local

--- core-java/src/com/problem/AnagramsTwoString.java
+++ core-java/src/com/problem/AnagramsTwoString.java
@@ -1,9 +1,30 @@
 package com.problem;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AnagramsTwoString {
+    private static int CHARACTER_RANGE= 256;
+
+    public static boolean   isAnagramCounting(String string1, String string2) {
+        if (string1.length() != string2.length()) {
+            return false;
+        }
+        int count[] = new int[CHARACTER_RANGE];
+        for (int i = 0; i < string1.length(); i++) {
+            count[string1.charAt(i)]++;
+            count[string2.charAt(i)]--;
+        }
+        System.out.println(Arrays.toString(count));
+        for (int i = 0; i < CHARACTER_RANGE; i++) {
+            if (count[i] != 0) {
+               // System.out.println(i+"vipul");
+                 return false;
+            }
+        }
+        return true;
+    }
     public static void isAnagrams(String s1, String s2) {
         if (s1.length() != s1.length()) {
             System.out.println("not anagrams");
@@ -37,6 +58,7 @@ public class AnagramsTwoString {
     }
 
     public static void main(String[] args) {
+        isAnagramCounting("table","tbael");
         isAnagrams("tea", "aee");
     }
 }
